5402ade4f3fae57258e3efdf1b5a3b1918685c0d
David Glick <dglick@salesforce.com>
2021-08-15 03:10:31 +0000

    Toggle favicon based on status

--- src/_includes/write.js
+++ src/_includes/write.js
@@ -1,5 +1,8 @@
 const write = (doc) => {
   const editor = document.getElementById('typr');
+  const favicon = document.querySelector("link[rel~='icon']");
+  const good_favicon_href = "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%90%B5%3C%2Ftext%3E%3C%2Fsvg%3E";
+  const bad_favicon_href = "data:image/svg+xml,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20100%20100%22%3E%3Ctext%20y%3D%22.9em%22%20font-size%3D%2290%22%3E%F0%9F%99%88%3C%2Ftext%3E%3C%2Fsvg%3E";
   let current = '';
 
   // the stats
@@ -53,9 +56,11 @@ const write = (doc) => {
 
     if (good) {
       fails = 0;
+      favicon.href = good_favicon_href;
       stats.hint = '';
     } else {
       fails += 1;
+      favicon.href = bad_favicon_href;
       stats.wasted += 1;
 
       if (fails > 10) {
